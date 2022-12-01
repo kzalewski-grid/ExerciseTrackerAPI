4abc5376919fa8f0e2cce8af974549a4904fe357
kzalewski <kzalewski@griddynamics.com>
2022-12-01 10:25:24 +0100

    Added query parameters error messages.

--- src/handlers/logs.ts
+++ src/handlers/logs.ts
@@ -2,8 +2,6 @@ import prisma from "../db";
 
 export const getLogs = async (req, res, next) => {
   try {
-    let from;
-    let to;
     const userId = +req.params.id;
     const user = await prisma.user.findUnique({
       where: {
@@ -25,18 +23,26 @@ export const getLogs = async (req, res, next) => {
     }
 
     if (req.query.from) {
-      from =
-        new Date(new Date(req.query.from).toDateString()).getTime() ||
-        Number.NEGATIVE_INFINITY;
+      const from = new Date(new Date(req.query.from).toDateString()).getTime();
+      if (isNaN(from)) {
+        res.status(400);
+        return res.json({
+          message: "`from` should be a date in yyyy-mm-dd format",
+        });
+      }
       user.log = user.log.filter((exercise) => {
         return new Date(exercise.date).getTime() >= from;
       });
     }
 
     if (req.query.to) {
-      to =
-        new Date(new Date(req.query.to).toDateString()).getTime() ||
-        Number.POSITIVE_INFINITY;
+      const to = new Date(new Date(req.query.to).toDateString()).getTime();
+      if (isNaN(to)) {
+        res.status(400);
+        return res.json({
+          message: "`to` should be a date in yyyy-mm-dd format",
+        });
+      }
       user.log = user.log.filter((exercise) => {
         return new Date(exercise.date).getTime() <= to;
       });
@@ -47,7 +53,11 @@ export const getLogs = async (req, res, next) => {
       (ex1, ex2) => new Date(ex1.date).getTime() - new Date(ex2.date).getTime()
     );
 
-    if (req.query.limit > 0) {
+    if (req.query.limit) {
+      if (!(req.query.limit > 0)) {
+        res.status(400);
+        return res.json({ message: "`limit` should be a positive number" });
+      }
       user.log = user.log.slice(0, req.query.limit);
     }
 
